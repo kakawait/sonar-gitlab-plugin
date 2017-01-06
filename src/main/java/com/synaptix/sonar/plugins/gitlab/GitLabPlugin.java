64bb0c0d941545c1a646a03f6f5f6dae9540ad4b
Thibaud Leprêtre <thibaud.lepretre@gmail.com>
2017-01-06 15:20:40 +0100

    Refactor GitLabPlugin

--- src/main/java/com/synaptix/sonar/plugins/gitlab/GitLabPlugin.java
+++ src/main/java/com/synaptix/sonar/plugins/gitlab/GitLabPlugin.java
@@ -26,63 +26,135 @@ import org.sonar.api.resources.Qualifiers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class GitLabPlugin extends SonarPlugin {
 
-    public static final String GITLAB_URL = "sonar.gitlab.url";
-    public static final String GITLAB_MAX_GLOBAL_ISSUES = "sonar.gitlab.max_global_issues";
-    public static final String GITLAB_USER_TOKEN = "sonar.gitlab.user_token";
-    public static final String GITLAB_PROJECT_ID = "sonar.gitlab.project_id";
-    public static final String GITLAB_COMMIT_SHA = "sonar.gitlab.commit_sha";
-    public static final String GITLAB_REF_NAME = "sonar.gitlab.ref_name";
-    public static final String GITLAB_IGNORE_FILE = "sonar.gitlab.ignore_file";
-    public static final String GITLAB_GLOBAL_TEMPLATE = "sonar.gitlab.global_template";
-    public static final String GITLAB_INLINE_TEMPLATE = "sonar.gitlab.inline_template";
-    public static final String GITLAB_COMMENT_NO_ISSUE = "sonar.gitlab.comment_no_issue";
-    public static final String GITLAB_IGNORE_SSL = "sonar.gitlab.ignore_ssl";
-    public static final String GITLAB_BUILD_INIT_STATE = "sonar.gitlab.build_init_state";
+    static final String GITLAB_URL = "sonar.gitlab.url";
+    static final String GITLAB_MAX_GLOBAL_ISSUES = "sonar.gitlab.max_global_issues";
+    static final String GITLAB_USER_TOKEN = "sonar.gitlab.user_token";
+    static final String GITLAB_PROJECT_ID = "sonar.gitlab.project_id";
+    static final String GITLAB_COMMIT_SHA = "sonar.gitlab.commit_sha";
+    static final String GITLAB_REF_NAME = "sonar.gitlab.ref_name";
+    static final String GITLAB_IGNORE_FILE = "sonar.gitlab.ignore_file";
+    static final String GITLAB_GLOBAL_TEMPLATE = "sonar.gitlab.global_template";
+    static final String GITLAB_INLINE_TEMPLATE = "sonar.gitlab.inline_template";
+    static final String GITLAB_COMMENT_NO_ISSUE = "sonar.gitlab.comment_no_issue";
+    static final String GITLAB_IGNORE_SSL = "sonar.gitlab.ignore_ssl";
+    static final String GITLAB_BUILD_INIT_STATE = "sonar.gitlab.build_init_state";
 
-    public static final String CATEGORY = "gitlab";
-    public static final String SUBCATEGORY = "reporting";
-    public static final List<String> BUILD_INIT_STATES = Arrays.asList("pending", "running");
+    static final List<String> BUILD_INIT_STATES = Collections.unmodifiableList(Arrays.asList("pending", "running"));
 
-    private static int serverIndex = 0;
+    private static final String CATEGORY = "gitlab";
+    private static final String INSTANCE_SUBCATEGORY = "instance";
+    private static final String REPORTING_SUBCATEGORY = "reporting";
 
     private static List<PropertyDefinition> definitions() {
-        return Arrays
-                .asList(PropertyDefinition.builder(GITLAB_URL).name("GitLab url").description("URL to access GitLab.").category(CATEGORY).subCategory(SUBCATEGORY).defaultValue("https://gitlab.com")
-                                .index(serverIndex++).build(),
-                        PropertyDefinition.builder(GITLAB_USER_TOKEN).name("GitLab User Token").description("GitLab user token is developer role.").category(CATEGORY).subCategory(SUBCATEGORY).index(serverIndex++)
-                                .build(), PropertyDefinition.builder(GITLAB_MAX_GLOBAL_ISSUES).name("GitLab Max Global Issues").description("Max issues to show in global comment.").category(CATEGORY)
-                                .subCategory(SUBCATEGORY).type(PropertyType.INTEGER).defaultValue(String.valueOf(10)).index(serverIndex++).build(),
-                        PropertyDefinition.builder(GITLAB_PROJECT_ID).name("GitLab Project id")
-                                .description("The unique id, path with namespace, name with namespace, web url, ssh url or http url of the current project that GitLab.").category(CATEGORY)
-                                .subCategory(SUBCATEGORY).index(serverIndex++).onlyOnQualifiers(Qualifiers.PROJECT).build(),
-                        PropertyDefinition.builder(GITLAB_COMMIT_SHA).name("GitLab Commit SHA").description("The commit revision for which project is built.").category(CATEGORY)
-                                .subCategory(SUBCATEGORY).index(serverIndex++).hidden().build(),
-                        PropertyDefinition.builder(GITLAB_REF_NAME).name("GitLab Ref Name").description("The commit revision for which project is built.").category(CATEGORY).subCategory(SUBCATEGORY)
-                                .index(serverIndex++).hidden().build(),
-                        PropertyDefinition.builder(GITLAB_IGNORE_FILE).name("GitLab Ingore file").description("Ignore issues on files no modified by the commit").category(CATEGORY)
-                                .subCategory(SUBCATEGORY).type(PropertyType.BOOLEAN).defaultValue(String.valueOf(false)).index(serverIndex++).hidden().build(),
-                        PropertyDefinition.builder(GITLAB_COMMENT_NO_ISSUE).name("GitLab Comment On No New Issue").description("Add a comment even when there is no new issue.").category(CATEGORY)
-                                .subCategory(SUBCATEGORY).type(PropertyType.BOOLEAN).defaultValue(String.valueOf(false)).index(serverIndex++).build(),
-                        PropertyDefinition.builder(GITLAB_IGNORE_SSL).name("GitLab ignore SSL.").description("Ignore certificate for https connections.").category(CATEGORY)
-                                .subCategory(SUBCATEGORY).type(PropertyType.BOOLEAN).defaultValue(String.valueOf(false)).index(serverIndex++).build(),
-                        PropertyDefinition.builder(GITLAB_BUILD_INIT_STATE).name("GitLab Build Initial State").description("Indicate what state should be the first when build commit status update is called.").category(CATEGORY).subCategory(SUBCATEGORY)
-                                .type(PropertyType.SINGLE_SELECT_LIST).options(BUILD_INIT_STATES).defaultValue("pending").index(serverIndex++).build()
-
-                        /*,
-                        PropertyDefinition.builder(GITLAB_GLOBAL_TEMPLATE).name("GitLab Global Template").description("Template for global comment in commit.").category(CATEGORY)
-                                .subCategory(SUBCATEGORY).type(PropertyType.TEXT).index(8).build(),
-                        PropertyDefinition.builder(GITLAB_INLINE_TEMPLATE).name("GitLab Inline Template").description("Template for inline comment in commit.").category(CATEGORY)
-                                .subCategory(SUBCATEGORY).type(PropertyType.TEXT).index(9).build()*/);
+        return Arrays.asList(
+                PropertyDefinition
+                        .builder(GITLAB_URL)
+                        .name("GitLab url")
+                        .description("URL to access GitLab. Default value is fine for public GitLab. " +
+                                "Can be modified for GitLab local installation.")
+                        .category(CATEGORY)
+                        .subCategory(INSTANCE_SUBCATEGORY)
+                        .defaultValue("https://gitlab.com")
+                        .index(1)
+                        .build(),
+                PropertyDefinition
+                        .builder(GITLAB_USER_TOKEN)
+                        .name("GitLab user token")
+                        .description("Token for the user that SonarQube will comment as.")
+                        .category(CATEGORY)
+                        .subCategory(INSTANCE_SUBCATEGORY)
+                        .index(2)
+                        .build(),
+                PropertyDefinition
+                        .builder(GITLAB_MAX_GLOBAL_ISSUES)
+                        .name("GitLab max global issues")
+                        .description("Max issues to show in global comment.")
+                        .category(CATEGORY)
+                        .subCategory(REPORTING_SUBCATEGORY)
+                        .type(PropertyType.INTEGER)
+                        .defaultValue(String.valueOf(10))
+                        .index(3)
+                        .build(),
+                PropertyDefinition
+                        .builder(GITLAB_PROJECT_ID)
+                        .name("GitLab Project id")
+                        .description("The unique id, path with namespace, name with namespace, web url, ssh url " +
+                                "or http url of the current project that GitLab.")
+                        .category(CATEGORY)
+                        .subCategory(REPORTING_SUBCATEGORY)
+                        .onlyOnQualifiers(Qualifiers.PROJECT)
+                        .index(4)
+                        .build(),
+                PropertyDefinition
+                        .builder(GITLAB_COMMIT_SHA)
+                        .name("Commit SHA")
+                        .description("The commit revision for which project is built.")
+                        .category(CATEGORY)
+                        .subCategory(REPORTING_SUBCATEGORY)
+                        .hidden()
+                        .index(5)
+                        .build(),
+                PropertyDefinition
+                        .builder(GITLAB_REF_NAME)
+                        .name("Ref name")
+                        .description("The commit revision for which project is built.")
+                        .category(CATEGORY)
+                        .subCategory(REPORTING_SUBCATEGORY)
+                        .hidden()
+                        .index(6)
+                        .build(),
+                PropertyDefinition
+                        .builder(GITLAB_IGNORE_FILE)
+                        .name("Ignore unrelated files")
+                        .description("Ignore issues on files not modified by the commit.")
+                        .category(CATEGORY)
+                        .subCategory(REPORTING_SUBCATEGORY)
+                        .type(PropertyType.BOOLEAN)
+                        .defaultValue(String.valueOf(false))
+                        .index(7)
+                        .build(),
+                PropertyDefinition
+                        .builder(GITLAB_COMMENT_NO_ISSUE)
+                        .name("Comment if no new issues")
+                        .description("Add a comment even when there is no new issue.")
+                        .category(CATEGORY)
+                        .subCategory(REPORTING_SUBCATEGORY)
+                        .type(PropertyType.BOOLEAN)
+                        .defaultValue(String.valueOf(false))
+                        .index(8)
+                        .build(),
+                PropertyDefinition
+                        .builder(GITLAB_IGNORE_SSL)
+                        .name("Ignore SSL")
+                        .description("Ignore SSL certificate for https GitLab connections.")
+                        .category(CATEGORY)
+                        .subCategory(INSTANCE_SUBCATEGORY)
+                        .index(9)
+                        .build(),
+                PropertyDefinition
+                        .builder(GITLAB_BUILD_INIT_STATE)
+                        .name("Build Initial State")
+                        .description("State that should be the first when build commit status update is called.")
+                        .category(CATEGORY)
+                        .subCategory(REPORTING_SUBCATEGORY)
+                        .index(10)
+                        .type(PropertyType.SINGLE_SELECT_LIST)
+                        .options(BUILD_INIT_STATES)
+                        .defaultValue("running")
+                        .build()
+        );
     }
 
     @Override
     public List getExtensions() {
         List extensions = new ArrayList();
-        extensions.addAll(Arrays.asList(CommitIssuePostJob.class, GitLabPluginConfiguration.class, CommitProjectBuilder.class, CommitFacade.class, InputFileCacheSensor.class, InputFileCache.class,
+        extensions.addAll(Arrays.asList(CommitIssuePostJob.class, GitLabPluginConfiguration.class,
+                CommitProjectBuilder.class, CommitFacade.class, InputFileCacheSensor.class, InputFileCache.class,
                 MarkDownUtils.class));
         extensions.addAll(definitions());
         return extensions;
